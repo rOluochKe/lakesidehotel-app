ff56b2bb9d9c5634a696583f89816ea1456d59fc
Raymond Oluoch <rasholuoch@gmail.com>
2024-03-25 14:19:57 +0300

    added components and booking form

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react"
+import MainHeader from "../layout/MainHeader"
+import HotelService from "../common/HotelService"
+import Parallax from "../common/Parallax"
+import RoomCarousel from "../common/RoomCarousel"
+// import RoomSearch from "../common/RoomSearch"
 
 const Home = () => {
-  return (
-    <div>
-      <h2>Welcome to LakeSide Hotel</h2>
-    </div>
-  )
+	return (
+		<section>
+			<MainHeader />
+			<div className="container">
+				<RoomCarousel />
+				<Parallax />
+				<RoomCarousel />
+				<HotelService />
+				<Parallax />
+				<RoomCarousel />
+			</div>
+		</section>
+	)
 }
 
 export default Home
