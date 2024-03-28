0e9eef48bc8dc7b367f3f341cb05dac9b29b21ea
Raymond Oluoch <rasholuoch@gmail.com>
2024-03-28 14:41:34 +0300

    auth and authirization

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -139,3 +139,81 @@ export async function getAvailableRooms(checkInDate, checkOutDate, roomType) {
 	)
 	return result
 }
+
+/* This function register a new user */
+export async function registerUser(registration) {
+	try {
+		const response = await api.post("/auth/register-user", registration)
+		return response.data
+	} catch (error) {
+		if (error.reeponse && error.response.data) {
+			throw new Error(error.response.data)
+		} else {
+			throw new Error(`User registration error : ${error.message}`)
+		}
+	}
+}
+
+/* This function login a registered user */
+export async function loginUser(login) {
+	try {
+		const response = await api.post("/auth/login", login)
+		if (response.status >= 200 && response.status < 300) {
+			return response.data
+		} else {
+			return null
+		}
+	} catch (error) {
+		console.error(error)
+		return null
+	}
+}
+
+/*  This is function to get the user profile */
+export async function getUserProfile(userId, token) {
+	try {
+		const response = await api.get(`users/profile/${userId}`, {
+			headers: getHeader()
+		})
+		return response.data
+	} catch (error) {
+		throw error
+	}
+}
+
+/* This isthe function to delete a user */
+export async function deleteUser(userId) {
+	try {
+		const response = await api.delete(`/users/delete/${userId}`, {
+			headers: getHeader()
+		})
+		return response.data
+	} catch (error) {
+		return error.message
+	}
+}
+
+/* This is the function to get a single user */
+export async function getUser(userId, token) {
+	try {
+		const response = await api.get(`/users/${userId}`, {
+			headers: getHeader()
+		})
+		return response.data
+	} catch (error) {
+		throw error
+	}
+}
+
+/* This is the function to get user bookings by the user id */
+export async function getBookingsByUserId(userId, token) {
+	try {
+		const response = await api.get(`/bookings/user/${userId}/bookings`, {
+			headers: getHeader()
+		})
+		return response.data
+	} catch (error) {
+		console.error("Error fetching bookings:", error.message)
+		throw new Error("Failed to fetch bookings")
+	}
+}
